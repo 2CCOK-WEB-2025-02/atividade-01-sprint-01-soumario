6661b35faaac029898fe57f6970991e4e31033ed
soumario <mario.aaraujojr@sptech.school>
2025-08-20 13:47:20 -0300

    Ex 01 finalizado

--- src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
@@ -1,13 +1,27 @@
 package school.sptech.sprint1_nota1.ex1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioFacilController {
 
     @GetMapping("/ex-01/{palavra}")
-    public Boolean exercicioFacil(String palavra) {
-        return null;
+    public Boolean exercicioFacil(@PathVariable String palavra) {
+
+        String palindromo = "";
+
+        for(int i = palavra.length() -1; i >= 0; i--){
+
+            palindromo += palavra.charAt(i);
+
+        }
+
+        if(!palindromo.toLowerCase().equals(palavra.toLowerCase())) {
+            return false;
+        }
+
+        return true;
     }
 }
--- src/main/java/school/sptech/sprint1_nota1/ex2/ExercicioMedioController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex2/ExercicioMedioController.java
@@ -1,13 +1,15 @@
 package school.sptech.sprint1_nota1.ex2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioMedioController {
 
     @GetMapping("/ex-02/{numero}")
-    public Boolean exercicioMedio(int numero) {
+    public Boolean exercicioMedio(@PathVariable Integer numero) {
+
         return null;
     }
 }
