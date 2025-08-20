d4da9bb1c9c2a972144e1c051d8b5052403fb142
soumario <mario.aaraujojr@sptech.school>
2025-08-20 13:53:30 -0300

    Ex 02 finalizado

--- src/main/java/school/sptech/sprint1_nota1/ex2/ExercicioMedioController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex2/ExercicioMedioController.java
@@ -10,6 +10,14 @@ public class ExercicioMedioController {
     @GetMapping("/ex-02/{numero}")
     public Boolean exercicioMedio(@PathVariable Integer numero) {
 
-        return null;
+        if (numero <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= numero; i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
