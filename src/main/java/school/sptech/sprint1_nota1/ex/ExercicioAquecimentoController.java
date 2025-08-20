6d970971c048ebdc31c456a90b103a33cb752e96
soumario <mario.aaraujojr@sptech.school>
2025-08-20 13:20:59 -0300

    Ex 00 finalizado

--- src/main/java/school/sptech/sprint1_nota1/ex/ExercicioAquecimentoController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex/ExercicioAquecimentoController.java
@@ -1,13 +1,19 @@
 package school.sptech.sprint1_nota1.ex;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioAquecimentoController {
 
     @GetMapping("/ex-00/{numero}")
-    public Boolean exercicioAquecimento(int numero) {
-        return null;
+    public Boolean exercicioAquecimento(@PathVariable Integer numero) {
+
+        if (numero % 2 != 0) {
+            return  false;
+        }
+
+        return true;
     }
 }
